bcc7f8aa662a3321c3eb827cf0163461e3f01eed
SHIVAM NASIT <109730470+ShivamTops2@users.noreply.github.com>
2022-07-26 17:18:33 +0530

    Add files via upload

---
+++ MY APPLICATION 8/main/java/com/example/myapplication/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity()
+{
+
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed(Runnable {
+
+            val i =Intent(this,LginActivity::class.java)
+            startActivity(i)
+
+        },3000)
+
+    }
+}
+
