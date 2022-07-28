732d3d3c2f3b66220dfec1f8ad7317ab3bf03659
SHIVAM NASIT <109730470+ShivamTops2@users.noreply.github.com>
2022-07-28 15:43:50 +0530

    Add files via upload

---
+++ MY APPLICATION 28/main/java/com/example/myapplication/LoginActivity.kt
@@ -0,0 +1,64 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+
+class LoginActivity : AppCompatActivity()
+{
+    lateinit var edt1:EditText
+    lateinit var edt2:EditText
+    lateinit var btn1:Button
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+        edt1=findViewById(R.id.edtname)
+        edt2=findViewById(R.id.edtpass)
+        btn1=findViewById(R.id.btn)
+
+
+        btn1.setOnClickListener()
+        {
+            var name=edt1.text.toString()
+            var pass=edt2.text.toString()
+
+            if(name.length==0 && pass.length==0)
+            {
+                edt1.setError("Enter your name")
+                edt2.setError("Enter your password")
+            }
+            else if(name.length==0)
+            {
+                edt1.setError("Enter your name")
+            }
+            else if(pass.length==0 && pass.length<8)
+            {
+                edt2.setError("Enter your password")
+            }
+            else
+            {
+                if(pass.equals("9120"))
+                {
+                    Toast.makeText(applicationContext,"Login Success", Toast.LENGTH_LONG).show()
+
+                    val i = Intent(this,MainActivity::class.java)
+                    startActivity(i)
+                }
+                else
+                {
+                    Toast.makeText(applicationContext,"Login Fail", Toast.LENGTH_LONG).show()
+                }
+
+            }
+
+        }
+    }
+}
---
+++ MY APPLICATION 28/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Toast
+import com.example.myapplication.R
+import java.lang.StringBuilder
+
+class MainActivity : AppCompatActivity()
+{
+    //declaration
+    lateinit var c1:CheckBox
+    lateinit var c2:CheckBox
+    lateinit var c3:CheckBox
+    lateinit var b1:Button
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //initialization
+        c1 =findViewById(R.id.chk1)
+        c2 =findViewById(R.id.chk2)
+        c3 =findViewById(R.id.chk3)
+        b1 =findViewById(R.id.btnorder)
+
+
+        //click event
+        b1.setOnClickListener()
+        {
+            var amount=0
+            var builder =StringBuilder()
+            builder.append("\n selected Items \n")
+
+            if(c1.isChecked)
+            {
+                amount+=100
+                builder.append("\n Pizza @rs.100 \n ")
+            }
+            if(c2.isChecked)
+            {
+                amount+=50
+                builder.append("\n Burger @rs.50 \n")
+            }
+            if(c3.isChecked)
+            {
+                amount+=120
+                builder.append("\n Coffee @rs.120 \n")
+            }
+
+            builder.append("\n Bill is \n"+amount)
+
+            //Toast.makeText(applicationContext," \n "+builder.toString(),Toast.LENGTH_LONG).show()
+
+
+            //putextra,getextra
+            val i =Intent(this,BillActivity::class.java)
+            i.putExtra("tops",builder.toString())
+            startActivity(i)
+
+
+        }
+
+
+    }
+
+}
